91da9eb7ceef6791fad415b000980928fcf7cd3c
Igor Nosovskyi <Igor.Nosovskyi@ubisoft.com>
2017-07-19 16:29:15 +0300

    Added hometask 6

--- HomeTaskTwoSix/HWTwoSix.cpp
+++ HomeTaskTwoSix/HWTwoSix.cpp
@@ -1,9 +1,18 @@
+//p.84
 #include <iostream>
 using namespace std;
+double calculation (double);
 int main ()
 {
-	int years;
+	double years;
+	cout << "Enter the number of light years: ";
 	cin >> years;
-	cout << years;
+	double astranomical = calculation(years);
+	cout << years << " light years = " << astranomical << " astronomical units" << endl;
 	system("pause");
+}
+
+double calculation (double c) 
+{
+	return c * 63240;
 }
