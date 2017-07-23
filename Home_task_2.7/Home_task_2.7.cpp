fd8c16a55b64570e18d52a32adc58a281165dfbb
Igor Nosovskyi <Igor.Nosovskyi@ubisoft.com>
2017-07-23 17:26:01 +0300

    Refactor all hometask

---
+++ Home_task_2.7/Home_task_2.7.cpp
@@ -0,0 +1,18 @@
+//page 84
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int print_hours;
+	int print_minutes;
+	cout << "Enter the number of hours: ";
+	cin >> print_hours;
+	cout << "Enter the number of minutes: ";
+	cin >> print_minutes;
+	cout << "Time: " << print_hours << " : " << print_minutes << endl;
+	
+	system("pause");
+
+	return 0;
+}
---
+++ lesson_2_oufrune/lesson_2_oufrune.cpp
@@ -0,0 +1,22 @@
+﻿// определение собственной функции
+#include <iostream>
+
+using namespace std;
+void simon(int); 
+
+int main()
+{
+	simon(3);
+	cout << "Pick an intenger: ";
+	int count;
+	cin >> count;
+	simon(count);
+	cout << "Done! " << endl;
+	system("pause");
+	return 0;
+}
+
+void simon(int n)
+{
+	cout << "Simon says touch yours toes " << n << " times" << endl;
+}
