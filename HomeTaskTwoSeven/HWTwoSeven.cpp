d898be0ad9db1951980cf97d0c4ca5e4a0a14169
Igor Nosovskyi <Igor.Nosovskyi@ubisoft.com>
2017-07-23 16:40:44 +0300

    Home task 2.7 done

--- HomeTaskTwoSeven/HWTwoSeven.cpp
+++ HomeTaskTwoSeven/HWTwoSeven.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 int main ()
 {
-	int printh;
-	int prints;
-	cout << "Enter the number of hours" << endl;
+	int print_hours;
+	int print_minutes;
+	cout << "Enter the number of hours: ";
+	cin >> print_hours;
+	cout << "Enter the number of minutes: ";
+	cin >> print_minutes;
+	cout << "Time: " << print_hours << " : " << print_minutes << endl;
 	
-
+	system("pause");
 
 	return 0;
 }
-
+/*
 void hours(int c)
 {
 	return 0;
@@ -21,4 +25,4 @@ void hours(int c)
 void seconds(int d)
 {
 	return 0;
-}
+}*/
