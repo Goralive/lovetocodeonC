e303354876e90f71528faba7ee24adce335dc1a9
Igor Nosovskyi <Igor.Nosovskyi@ubisoft.com>
2017-10-04 09:43:51 +0300

    Added home tasks

---
+++ Home_task_3.2/home_task_3.2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	short sh = 80;
+	unsigned int ung = 42110;
+	long long lo = 3000000000;
+
+}
---
+++ Home_task_3.6/home_task_3.6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	char letter = 88;
+
+	std::cout << "The char 88 equals to letter: " << letter << "\n";
+	
+	std::cout.put(char(88)) << "\n";
+	
+	std::cout << char(88) << "\n";
+
+	std::cin.get();
+
+
+
+	return 0;
+
+}
