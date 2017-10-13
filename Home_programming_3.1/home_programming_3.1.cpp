23ef6aba0c969365091787a09f794483182f2958
Igor Nosovskyi <Igor.Nosovskyi@ubisoft.com>
2017-10-13 22:25:47 +0300

    Working with home task 3.1

--- Home_programming_3.1/home_programming_3.1.cpp
+++ Home_programming_3.1/home_programming_3.1.cpp
@@ -1,3 +1,16 @@
 // p.129
-// Create program wich ask height in inches and change it in foot and inches
+// Create program wich ask height in inches and change it in feet and inches
 #include <iostream>
+int main()
+{
+	double heigh;
+	std::cout << "Enter your height in inches and you will get it in foot and inches:  \n";
+	std::cin >> heigh;
+	double feet = heigh / 12;
+	// listening 3.12
+	std::cout << "Here is the result: " << feet << " feet" ;
+
+	std::cin.get();
+	std::cin.get();
+	return 0;
+}
