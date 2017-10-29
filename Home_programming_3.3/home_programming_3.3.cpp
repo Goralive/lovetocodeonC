8dec80e704a4f7e88377287e2c5debf7e1663949
Goralive <nosovskyigor@gmail.com>
2017-10-29 23:18:12 +0300

    WIP home task 3.3

--- Home_programming_3.3/home_programming_3.3.cpp
+++ Home_programming_3.3/home_programming_3.3.cpp
@@ -1,6 +1,31 @@
-/*p. 129 Programm which ask latitude in degree, minutes and seconds, after that shows  */
+//p. 129 Programm which ask latitude in degree, minutes and seconds, after that shows latitude in decimal format 
 #include <iostream>
 int main()
 {
+	int degree;
+	int minutes;
+	int seconds;
+	double latitude;
+	
+	
+	std::cout << "Enter a latitude in degrees, minutes, and seconds:\n";
+	std::cout << "First, enter the degrees: ";
+	std::cin >> degree;
 
+	std::cout << "Next, enter the minutes of arc: ";
+	std::cin >> minutes;
+
+	std::cout << "Finally, enter the second of arc: ";
+	std::cin >> seconds;
+
+// Finish it!!!!	latitude = 
+
+	std::cout << degree << " degrees, " << minutes << " minutes," << seconds << " seconds = " << latitude << " degrees";
+
+	
+
+	std::cin.get();
+	std::cin.get();
+
+	return 0;
 }
